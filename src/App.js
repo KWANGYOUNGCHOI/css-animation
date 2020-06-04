62e33ed90e49e48848b01e752d294a89c078150d
KWANGYOUNGCHOI <cky1203@gmail.com>
2020-06-04 23:21:48 +0900

    checkout git

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out git
         </a>
       </header>
     </div>
