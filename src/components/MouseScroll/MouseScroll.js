6d11997c339289ed5b1384490598efd54442f354
KWANGYOUNGCHOI <cky1203@gmail.com>
2020-06-05 15:07:15 +0900

    add MouseScroll

---
+++ src/components/MouseScroll/MouseScroll.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./MouseScroll.css";
+
+function MouseScroll() {
+  return (
+    <div className="mouse-scroll-c">
+      <div className="down"></div>
+    </div>
+  );
+}
+
+export default MouseScroll;
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -6,6 +6,7 @@ import Waterwheel from '../components/Waterwheel/Waterwheel';
 import EyeFollow from '../components/EyeFollow/EyeFollow';
 import NeonLine from '../components/NeonLine/NeonLine';
 import RoadRail from '../components/RoadRail/RoadRail';
+import MouseScroll from '../components/MouseScroll/MouseScroll';
 
 function Layout() {
   return (
@@ -16,6 +17,7 @@ function Layout() {
       <EyeFollow></EyeFollow>
       <NeonLine></NeonLine>
       <RoadRail></RoadRail>
+      <MouseScroll></MouseScroll>
     </Container>
   );
 }
